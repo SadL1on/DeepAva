35647597aa322047b8a8a4635dd7e99ef4b5d1d2
SadLion(Home) <leva51999@mail.ru>
2020-04-13 23:02:54 +0500

    DeadEnd

---
+++ KursAuth/KursAuth/Views/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using Avalonia.Interactivity;
+using KursAuth.Models;
+using KursAuth.ViewModels;
+using System;
+
+namespace KursAuth.Views
+{
+    public class MainWindow : ReactiveWindow<MainWindowViewModel>
+    {
+        public Button vkOpen;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            vkOpen = this.FindControl<Button>("VkOpen");
+            vkOpen.Click += VkOpen_Click;
+        }
+
+        private void VkOpen_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.IsVisConCtrl = !(ViewModel.IsVisConCtrl);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+        }
+
+    }
+}
---
+++ KursAuth/KursAuth/Views/rrr.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KursAuth.Views
+{
+    public class rrr : UserControl
+    {
+        public rrr()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
